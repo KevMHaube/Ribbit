87592948adff94ce65e9a87ee9d0202aaba4e178
Kevin Haube <kevinhaube@kevins-mbp.home>
2014-09-27 11:56:59 -0400

    Initial commit

---
+++ app/src/main/java/com/example/kevin/ribbit/FriendsFragment.java
@@ -0,0 +1,85 @@
+package com.example.kevin.ribbit;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+/**
+ * Created by Kevin on 9/16/2014.
+ */
+public class FriendsFragment extends ListFragment {
+
+    protected List<ParseUser> mFriends;
+    protected ParseRelation<ParseUser> mFriendsRelation;
+    protected ParseUser mCurrentUser;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_friends, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        mCurrentUser = ParseUser.getCurrentUser();
+        mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
+
+        getActivity().setProgressBarIndeterminateVisibility(true);
+
+        ParseQuery<ParseUser> query = mFriendsRelation.getQuery();
+        query.addAscendingOrder(ParseConstants.KEY_USERNAME);
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> parseUsers, ParseException e) {
+                getActivity().setProgressBarIndeterminateVisibility(false);
+
+            }
+        });
+
+        mFriendsRelation.getQuery().findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> parseUsers, ParseException e) {
+
+                if(e == null) {
+                    mFriends = parseUsers;
+
+                    String[] usernames = new String[mFriends.size()];
+                    int i = 0;
+                    for (ParseUser parseUser : mFriends) {
+                        usernames[i] = parseUser.getUsername();
+                        i++;
+                    }
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getListView().getContext(),
+                            android.R.layout.simple_list_item_1, usernames);
+                    setListAdapter(adapter);
+                } else {
+                    AlertDialog();
+                }
+            }
+        });
+    }
+
+    private void AlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getListView().getContext());
+        builder.setTitle(getString(R.string.error_title))
+                .setMessage(getString(R.string.error_unexpected))
+                .setPositiveButton(android.R.string.ok, null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+}
