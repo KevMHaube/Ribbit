07c0f65db03d0422fcace04981c5a67197ef5beb
kevmhaube <kevinhaube@outlook.com>
2014-09-30 14:17:29 -0400

    Feature: Swipe to refresh Inbox

--- app/src/main/java/com/example/kevin/ribbit/ui/InboxFragment.java
+++ app/src/main/java/com/example/kevin/ribbit/ui/InboxFragment.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.kevin.ribbit.adapters.MessageAdapter;
 import com.example.kevin.ribbit.utils.ParseConstants;
@@ -28,11 +30,18 @@ import java.util.List;
 public class InboxFragment extends ListFragment {
 
     protected List<ParseObject> mMessages;
+    protected SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_inbox, container, false);
+        mSwipeRefreshLayout = (SwipeRefreshLayout)rootView.findViewById(R.id.swipeRefreshLayout);
+        mSwipeRefreshLayout.setOnRefreshListener(mOnRefreshListener);
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.swipe_refresh_1,
+                R.color.swipe_refresh_2,
+                R.color.swipe_refresh_3,
+                R.color.swipe_refresh_4);
         return rootView;
     }
 
@@ -41,6 +50,10 @@ public class InboxFragment extends ListFragment {
         super.onResume();
         getActivity().setProgressBarIndeterminateVisibility(true);
 
+        RetrieveMessages();
+    }
+
+    private void RetrieveMessages() {
         ParseQuery<ParseObject> query = new ParseQuery<ParseObject>(ParseConstants.CLASS_MESSAGES);
         query.whereEqualTo(ParseConstants.KEY_RECIPIENT_IDS, ParseUser.getCurrentUser().getObjectId());
         query.addDescendingOrder(ParseConstants.KEY_CREATED_AT);
@@ -49,6 +62,10 @@ public class InboxFragment extends ListFragment {
             @Override
             public void done(List<ParseObject> messages, ParseException e) {
                 getActivity().setProgressBarIndeterminateVisibility(false);
+
+                if (mSwipeRefreshLayout.isRefreshing()){
+                    mSwipeRefreshLayout.setRefreshing(false);
+                }
 
                 if (e == null) {
                     mMessages = messages;
@@ -108,4 +125,11 @@ public class InboxFragment extends ListFragment {
             message.saveInBackground();
         }
     }
+
+    protected SwipeRefreshLayout.OnRefreshListener mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+            RetrieveMessages();
+        }
+    };
 }
